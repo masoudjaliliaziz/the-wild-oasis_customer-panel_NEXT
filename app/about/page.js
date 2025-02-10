e4f86fd9c61a51c5a09b9583daefe6fa9e43b04b
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-02-10 16:44:33 +0330

    complete the metadata of each page and the icon of that

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
 export default function Page() {
   return <h1>about the world oasis</h1>;
 }
--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,12 @@ import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "welcome / The Wild Oasis ",
+  },
+  description:
+    "Luxurios cabin hotel,located in the heart of Italian Dolomities, surrounded by beatifull mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
