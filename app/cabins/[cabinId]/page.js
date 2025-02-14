6bb5cb1bcba385bdc06df6282bba6fa1768b068e
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-02-14 11:44:31 +0330

    debug the title of metadata for each cabin page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,11 +2,16 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+export async function generateMetadata({params}){
+  const {name}=await getCabin(params.cabinId);
+  return {title:`Cabin ${name}`}
+}
+
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
-
   return (
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
