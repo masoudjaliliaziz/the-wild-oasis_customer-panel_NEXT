4bbf640d3947f06bc0d7383f592d25cf92e6a9bc
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-03-10 09:29:23 +0330

    add the filter component by using url to intraction between client componnent and server copmonent

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,12 +1,26 @@
 import { getCabins } from "../_lib/data-service";
 import CabinCard from "./CabinCard";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   const cabins = await getCabins();
   if (!cabins) return;
+
+  //conditionally rendering the cabin list by using filter that comes from searchParams tha equal by capacity
+  let displayedCabins;
+  if (filter === "all") displayedCabins = cabins;
+  if (filter === "small")
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  if (filter === "medium")
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  if (filter === "large")
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+
+  //render the finall filtered cabin list
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
